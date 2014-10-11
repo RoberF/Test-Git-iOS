10cebd671e63d1ff08f4781052780d7308b56529
Rober <rober@iMac-de-Lara.local>
2014-10-11 20:20:37 +0200

    added a test class

---
+++ RFTest.h
@@ -0,0 +1,13 @@
+//
+//  RFTest.h
+//  Test Git Project
+//
+//  Created by Rober on 11/10/14.
+//  Copyright (c) 2014 RoberF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RFTest : NSObject
+
+@end
---
+++ RFTest.m
@@ -0,0 +1,13 @@
+//
+//  RFTest.m
+//  Test Git Project
+//
+//  Created by Rober on 11/10/14.
+//  Copyright (c) 2014 RoberF. All rights reserved.
+//
+
+#import "RFTest.h"
+
+@implementation RFTest
+
+@end
